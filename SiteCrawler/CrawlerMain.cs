ba165aa5d28504238a3c04ae3afe8ac5b6ee5567
Tania Leonova-Vendrovska <Tania Leonova-Vendrovska>
2017-06-17 23:15:25 -0700

    initial http get request  works

---
+++ SiteCrawler/CrawlerMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteCrawler
+{
+    class CrawlerMain
+    {
+        static void Main(string[] args)
+        {
+            CrawlerMain c = new CrawlerMain();
+            c.getCall();
+
+        }
+         public void getCall()
+        {
+            Task t =  httpGetUrl();
+            t.Wait(); 
+        }
+
+        async private Task httpGetUrl()
+        {
+            HttpClient client = new HttpClient();
+            var responseString = await client.GetStringAsync("https://www.testingmom.com");
+            Console.Write("hi");
+        }
+    }
+}
